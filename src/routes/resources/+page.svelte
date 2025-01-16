55d94d5eeffb7aa82f74751e87ff2d031b1c5508
Brianna Wilson <bronna@gmail.com>
2025-01-16 11:47:47 -0800

    resources

--- src/routes/resources/+page.svelte
+++ src/routes/resources/+page.svelte
@@ -3,11 +3,30 @@
 
 <h1>Resources</h1>
 
-<h2 class="text-width">Coming soon</h2>
+<div class="text-width resources">
+    <div class="link">
+        <a href="https://factoregon.org/" target="_blank">FACT Oregon</a>
+        <p>Has a support line for families to ask questions about navigating systems and getting the resources their child needs. They also have courses for parents, and so much more.</p>
+    </div>
+    <div class="link">
+        <a href="https://www.droregon.org/" target="_blank">Disability Rights Oregon</a>
+        <p>Does legal advocacy work for people of all ages with disabilities, including children and students. Some of their work has contributed to passing legislation that makes it illegal for disabled students to be denied the right to attend school for the full day, and to end restraint and seclusion practices in schools.</p>
+    </div>
+    <div class="link">
+        <a href="https://sites.ed.gov/idea/" target="_blank">Individuals with Disabilities Act (IDEA)</a>
+        <p>The law enacted to make sure all children have the right to a free and appropriate public education nationwide. IDEA also specifies that children with disabilities have the right to be educated in the 'least restrictive environment,' or, in other words, as mainstream an environment as possible with needed supports.</p>
+    </div>
+</div>
 
 <style>
-    h2 {
-        margin-top: 3rem;
-        margin-bottom: 3rem;
+    .resources {
+        margin-top: 4rem;
+        margin-bottom: 4rem;
+    }
+
+    .link a {
+        font-weight: bold;
+        color: var(--colorInclusiveDark);
+        font-size: 1.2rem;
     }
 </style>
