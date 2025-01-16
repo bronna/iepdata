f786a3ea56f6e3104eec47c85f043bc85b40f2f5
Brianna Wilson <bronna@gmail.com>
2025-01-16 11:10:40 -0800

    contact info

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -3,11 +3,21 @@
 
 <h1>Contact</h1>
 
-<h2 class="text-width">Coming soon</h2>
+<div class="text-width contact">
+    <p>
+        We're always looking to improve these tools to make them more helpful and accessible! We'd love to hear from you.
+    </p>
+    <p>
+        You can contact us by email at <strong>disabilityeddata@protonmail.com</strong>, or by using the <strong>'Give Feedback' button</strong> in the bottom right corner below.
+    </p>
+    <p>
+        Thank you!
+    </p>
+</div>
 
 <style>
-    h2 {
-        margin-top: 3rem;
-        margin-bottom: 3rem;
+    .contact {
+        margin-top: 4rem;
+        margin-bottom: 4rem;
     }
 </style>
