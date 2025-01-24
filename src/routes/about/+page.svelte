84deaf93c344ef2fa728db070d6388c2d328e80f
Brianna Wilson <bronna@gmail.com>
2025-01-24 13:12:31 -0800

    about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,31 +1,22 @@
 <script>
-    import Scatterplot from '$lib/components/Scatterplot.svelte'
-    import BubbleMap from '$lib/components/BubbleMap.svelte'
+
+
 </script>
 
 <h1>About</h1>
 
 <div class="text-width about">
     <p>
-        This is a project to make data about disabled students in Oregon more accessible and understandable for families, students, and advocates. Brianna started it after talking to several principals of elementary schools in two different states about what her then-baby's grade school experience would look like. She was disturbed by the lack of transparency and consistency in the supports that disabled students receive in different areas, and set out to find data that could help give her a better sense of things. She soon realized this data could be helpful to others as well, and started this site as a way to visualize and format that data into accessible tools.
+        This project is about making data for disabled students in Oregon more accessible and understandable. Brianna started working on it after talking to several principals of elementary schools about what her child's grade school experience would be like. She was disturbed by the lack of transparency and consistency in supports that disabled students receive in different areas, and set out to find the data that could help give a better sense of things. She started this site as a way to visualize and format that data into accessible tools for families, students, and advocates.
     </p>
+
+    <img src="$lib/images/about.jpg" alt="about" style="width: 100px; height: auto; margin-top: 2rem; margin-bottom: 2rem;">
 </div>
 
-<div class="text-width map-container" style="--map-width: {500}px; --map-height: {500}px;">
-    <p>(don't mind these, just some current visualization tests)</p>
-    <Scatterplot />
-    <BubbleMap />
-</div>
 
 <style>
     .about {
         margin-top: 4rem;
         margin-bottom: 4rem;
-    }
-
-    .map-container {
-        margin: 0 auto;
-        width: 100%;
-        height: 100%;
     }
 </style>
---
+++ src/routes/viz-tests/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    import Scatterplot from '$lib/components/Scatterplot.svelte'
+    import BubbleMap from '$lib/components/BubbleMap.svelte'
+</script>
+
+<h1>Laboratory</h1>
+
+<div class="text-width map-container" style="--map-width: {500}px; --map-height: {500}px;">
+    <Scatterplot />
+    <BubbleMap />
+</div>
+
+<style>
+    .map-container {
+        margin: 0 auto;
+        width: 100%;
+        height: 100%;
+    }
+</style>
