b3bcea9fffec18aef7f20423b9b803f9b937f34c
Brianna Wilson <bronna@gmail.com>
2025-04-15 13:16:50 -0700

    funding text

--- src/routes/funding/+page.svelte
+++ src/routes/funding/+page.svelte
@@ -33,11 +33,6 @@
     </div>
 
     <div class="content-container">
-        <div class="text-width first-text">
-            <p> 
-                Oregon's special education funding cap falls significantly short of meeting the needs of school districts across the state. Many of Oregon's largest districts identify well above the 11% threshold, with some of the largest districts finding that 18% of their students need Individualized Education Plans (IEPs). Those additional services go unfunded by the state.
-            </p>
-        </div>
 
         <div class="map-container">
             <div>
@@ -50,11 +45,15 @@
         </div>
 
         <div class="text-width last-text">
+            <p> 
+                Oregon's special education funding cap falls significantly short of meeting the needs of school districts across the state. The majority of Oregon's districts fall well above the threshold, with many more than 11% of their students needing Individualized Education Plans (IEPs).
+            </p>
+
             <h3>
                 What does this mean?
             </h3>
             <p>
-                This funding structure creates a problematic disincentive for districts to identify students with disabilities. When districts identify students beyond the 11% cap, they must absorb those additional costs without corresponding state support. Despite the disincentive, districts are still identifying many more than 11% of their students needing supports. And if students don't get the supports they need early-on there can be devastating (and even more expensive) effects down the road.
+                This funding structure creates a disincentive for districts to identify students with disabilities. When districts identify students beyond the 11% cap, they must absorb costs not funded by the state. Despite this disincentive, districts are still identifying many more students as needing supports.
             </p>
             <h3>
                 How does this compare to other states?
@@ -66,7 +65,7 @@
                 Next steps
             </h3>
             <p>
-                Talk to your legislators (find them <a href="https://geo.maps.arcgis.com/apps/instant/lookup/index.html?appid=fd070b56c975456ea2a25f7e3f4289d1" target="_blank">here</a>). There are a couple of bills moving through the state legislature right now, one to <a href="https://olis.oregonlegislature.gov/liz/2025R1/Measures/Overview/HB2953" target="_blank">remove the special education funding cap</a>, and one to <a href="https://olis.oregonlegislature.gov/liz/2025R1/Measures/Overview/HB2448" target="_blank">increase funds to the High Cost Disabilities Account</a>. Tell your legislators that you support these bills and want to see them passed.
+                Talk to your legislators (find them <a href="https://geo.maps.arcgis.com/apps/instant/lookup/index.html?appid=fd070b56c975456ea2a25f7e3f4289d1" target="_blank">here</a>). There is a bill moving through the state legislature to <a href="https://olis.oregonlegislature.gov/liz/2025R1/Measures/Overview/HB2953" target="_blank">remove the special education funding cap</a>. Another one would <a href="https://olis.oregonlegislature.gov/liz/2025R1/Measures/Overview/HB2448" target="_blank">increase funds to the High Cost Disabilities Account</a>. Tell your legislators that you support these bills and want to see them passed.
             </p>
         </div>
 
